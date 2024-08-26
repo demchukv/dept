0252a2c8534e8ae8c88e3ec3cafe3884afccc1e6
Volodymyr Demchuk <demchukv@gmail.com>
2024-08-26 22:31:21 +0300

    start bottom tabs and accordion

--- app/components/dashboard/tabs-left.tsx
+++ app/components/dashboard/tabs-left.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import { Card } from '@/app/components/card/card';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 
 export const TabsLeft = () => {
   return (
-    <Card>
-      <div>TabsLeft 1</div>
-      <div>TabsLeft 2 </div>
-      <div>TabsLeft </div>
-    </Card>
+    <Tabs defaultValue="tab1" className="w-full">
+      <TabsList>
+        <TabsTrigger value="tab1">Покупки</TabsTrigger>
+        <TabsTrigger value="tab2">Заявки/задачі</TabsTrigger>
+        <TabsTrigger value="tab3">Ремонт техніки</TabsTrigger>
+      </TabsList>
+      <TabsContent value="tab1">
+        <Card>TabsLeft 1</Card>
+      </TabsContent>
+      <TabsContent value="tab2">
+        <Card>TabsLeft 2</Card>
+      </TabsContent>
+      <TabsContent value="tab3">
+        <Card>TabsLeft 3</Card>
+      </TabsContent>
+    </Tabs>
   );
 };
--- app/components/dashboard/tabs-right.tsx
+++ app/components/dashboard/tabs-right.tsx
@@ -1,14 +1,32 @@
 import React from 'react';
 import { Card } from '@/app/components/card/card';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 
 export const TabsRight = () => {
   return (
-    <Card>
-      <div>TabsRight 1</div>
-      <div>TabsRight 2 </div>
-      <div>TabsRight 3 </div>
-      <div>TabsRight 4</div>
-      <div>TabsRight 5</div>
-    </Card>
+    <Tabs defaultValue="tab1" className="w-full">
+      <TabsList>
+        <TabsTrigger value="tab1">Підписки</TabsTrigger>
+        <TabsTrigger value="tab2">Сервери</TabsTrigger>
+        <TabsTrigger value="tab3">Домени</TabsTrigger>
+        <TabsTrigger value="tab4">SSL-Сертифікати</TabsTrigger>
+        <TabsTrigger value="tab5">Номери</TabsTrigger>
+      </TabsList>
+      <TabsContent value="tab1">
+        <Card>TabsRight 1</Card>
+      </TabsContent>
+      <TabsContent value="tab2">
+        <Card>TabsRight 2</Card>
+      </TabsContent>
+      <TabsContent value="tab3">
+        <Card>TabsRight 3</Card>
+      </TabsContent>
+      <TabsContent value="tab4">
+        <Card>TabsRight 4</Card>
+      </TabsContent>
+      <TabsContent value="tab5">
+        <Card>TabsRight 5</Card>
+      </TabsContent>
+    </Tabs>
   );
 };
