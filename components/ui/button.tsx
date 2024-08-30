92339be3f55eea1908c5913559903fae76f374df
Volodymyr Demchuk <demchukv@gmail.com>
2024-08-30 18:03:34 +0300

    change global styles

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -5,21 +5,19 @@ import { cva, type VariantProps } from 'class-variance-authority';
 import { cn } from '@/lib/utils';
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-slate-950 disabled:pointer-events-none disabled:opacity-50 dark:focus-visible:ring-slate-300',
+  'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-slate-950 disabled:pointer-events-none disabled:opacity-50',
   {
     variants: {
       variant: {
         default:
           'bg-main-color text-white rounded-[4px] pt-3 pb-[13px] px-5 font-bold text-sm border border-main-color hover:shadow-md disabled:border-gray-medium disabled:text-gray-medium disabled:bg-white',
-        destructive:
-          'bg-red-500 text-slate-50 shadow-sm hover:bg-red-500/90 dark:bg-red-900 dark:text-slate-50 dark:hover:bg-red-900/90',
+        destructive: 'bg-red-500 text-slate-50 shadow-sm hover:bg-red-500/90',
         outline:
           'bg-white border border-main-color rounded-[4px] pt-3 pb-[13px] px-5 text-main-color font-bold text-sm hover:shadow-md disabled:border-gray-medium disabled:text-gray-medium',
         secondary:
-          'bg-slate-100 text-slate-900 shadow-sm hover:bg-slate-100/80 dark:bg-slate-800 dark:text-slate-50 dark:hover:bg-slate-800/80',
-        ghost:
-          'hover:bg-slate-100 hover:text-slate-900 dark:hover:bg-slate-800 dark:hover:text-slate-50',
-        link: 'text-slate-900 underline-offset-4 hover:underline dark:text-slate-50',
+          'bg-slate-100 text-slate-900 shadow-sm hover:bg-slate-100/80',
+        ghost: 'hover:bg-slate-100 hover:text-slate-900 ',
+        link: 'text-slate-900 underline-offset-4 hover:underline',
         hidden: 'focus-visible:outline-none',
         pagination: 'font-medium text-sm text-main-dark leading-main-lh',
         paginationActive:
