5323cc3bc4f168baa97ace97f9655c7c402f0327
Volodymyr Demchuk <demchukv@gmail.com>
2024-08-31 16:04:16 +0300

    balance page layout

--- app/components/balance/balance-dashboard.tsx
+++ app/components/balance/balance-dashboard.tsx
@@ -2,16 +2,18 @@
 import { useAppSelector } from '@/store/hooks';
 import { selectCurrentAccount } from '@/store/account/accountSlice';
 
+import { Card } from '@/app/components/card/card';
+
 export const BalanceDashboard = () => {
   const currentAccount = useAppSelector(selectCurrentAccount);
 
   return (
-    <div>
-      <div>Баланс</div>
-      <div>Додані картки</div>
-      <div>Історія операцій</div>
+    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:gap-[30px]">
+      <Card>Баланс</Card>
+      <Card>Додані картки</Card>
+      <Card>Історія операцій</Card>
       {currentAccount?.account === 'company' && (
-        <div>Рахунки (для компаній)</div>
+        <Card>Рахунки (для компаній)</Card>
       )}
     </div>
   );
