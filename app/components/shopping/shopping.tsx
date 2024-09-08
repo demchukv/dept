5de98064475e25dadb0b13cf03373e9b6b4ee715
Volodymyr Demchuk <demchukv@gmail.com>
2024-09-08 11:36:29 +0300

    add tracker component

--- app/components/shopping/shopping.tsx
+++ app/components/shopping/shopping.tsx
@@ -6,7 +6,27 @@ export const Shopping = () => {
       <h1 className="font-bold text-2xl leading-none text-main-dark mb-4 lg:mb-8">
         Покупки
       </h1>
-      <Tracker />
+      <Tracker
+        state={3}
+        data={[
+          { id: 1, date: '10.03.2024', name: 'Обробляється' },
+          {
+            id: 2,
+            date: '11.03.2024',
+            name: 'Відправлено',
+          },
+          {
+            id: 3,
+            date: '14.03.2024',
+            name: 'Очікує отримувача',
+          },
+          {
+            id: 4,
+            date: '20.03.2024',
+            name: 'Виконано',
+          },
+        ]}
+      />
     </>
   );
 };
--- app/components/shopping/tracker.tsx
+++ app/components/shopping/tracker.tsx
@@ -1,71 +1,124 @@
 import { cn } from '@/lib/utils';
+import React from 'react';
 
-export const Tracker = () => {
-  const stateList = [
-    {
-      id: 1,
-      name: 'Обробляється',
-      color: 'attention',
-    },
-    {
-      id: 2,
-      name: 'Відправлено',
-      color: 'blue-additional-color',
-    },
-    {
-      id: 3,
-      name: 'Очікує отримувача',
-      color: 'orange-additional-color',
-    },
-    {
-      id: 4,
-      name: 'Виконано',
-      color: 'green-additional-color',
-    },
-  ];
-  const defColor = 'gray-medium';
+const stateList: stateType[] = [
+  {
+    id: 1,
+    name: 'Обробляється',
+  },
+  {
+    id: 2,
+    name: 'Відправлено',
+  },
+  {
+    id: 3,
+    name: 'Очікує отримувача',
+  },
+  {
+    id: 4,
+    name: 'Виконано',
+  },
+];
+
+type stateType = {
+  id: number;
+  name: string;
+  date?: string | null;
+};
+interface TrackerProps {
+  state: number;
+  data: stateType[];
+}
+export const Tracker = ({ state, data }: TrackerProps) => {
   return (
     <div>
       <div className="grid grid-cols-1 gap-0.5 sm:grid-cols-[24px_auto_24px_auto_24px_auto_24px]">
-        <TrackerPoint color="attention" outer={false} />
-        <TrackerLine color="attention" />
-        <TrackerPoint color="blue-additional-color" outer={false} />
-        <TrackerLine color="blue-additional-color" />
-        <TrackerPoint color="orange-additional-color" outer={false} />
-        <TrackerLine color="orange-additional-color" />
-        <TrackerPoint color="green-additional-color" outer={true} />
+        {data.map((item, ind) => (
+          <React.Fragment key={item.id}>
+            {ind > 0 && <TrackerLine state={state} id={item.id} />}
+            <TrackerPoint
+              name={item.name}
+              state={state}
+              id={item.id}
+              date={item.date}
+            />
+          </React.Fragment>
+        ))}
       </div>
     </div>
   );
 };
 
 interface TrackerPointProps {
-  color: string;
-  outer: boolean;
+  name: string;
+  state: number;
+  id: number;
+  date?: string | null;
 }
-const TrackerPoint = ({ color, outer }: TrackerPointProps) => {
+const TrackerPoint = ({ name, state, id, date }: TrackerPointProps) => {
   return (
-    <div
-      className={cn(
-        'w-6 h-6 rounded-full border grid place-content-center',
-        outer ? `border-${color}` : 'border-transparent',
+    <div className="grid grid-cols-[24px_auto] gap-1 sm:grid-cols-1 relative">
+      <div
+        className={cn(
+          'w-6 h-6 rounded-full border grid place-items-center border-transparent',
+          state === 1 && state === id && 'border-attention',
+          state === 2 && state === id && 'border-blue-additional-color',
+          state === 3 && state === id && 'border-orange-additional-color',
+          state === 4 && state === id && 'border-green-additional-color',
+        )}
+      >
+        <div
+          className={cn(
+            'w-3 h-3 rounded-full bg-gray-medium',
+            state === 1 && state >= id && 'bg-attention',
+            state === 2 && state >= id && 'bg-blue-additional-color',
+            state === 3 && state >= id && 'bg-orange-additional-color',
+            state === 4 && state >= id && 'bg-green-additional-color',
+          )}
+        ></div>
+      </div>
+      {state >= id && (
+        <div
+          className={cn(
+            'flex gap-2 items-center ',
+            id === 1 && 'sm:absolute sm:left-0 sm:top-7',
+            id > 1 && id < 4 && 'sm:absolute sm:left-[-80%] sm:top-7',
+            id === 4 && 'sm:absolute sm:right-0 sm:top-7',
+          )}
+        >
+          {date && (
+            <span className="text-sm font-normal text-main-dark leading-main-lh">
+              {date}
+            </span>
+          )}
+          <span
+            className={cn(
+              'sm:hidden text-sm font-medium text-gray-dark leading-main-lh',
+              state === id && 'font-semibold',
+            )}
+          >
+            {name}
+          </span>
+        </div>
       )}
-    >
-      <div className={cn('w-3 h-3 rounded-full', `bg-${color}`)}></div>
     </div>
   );
 };
 
 interface TrackerLineProps {
-  color: string;
+  state: number;
+  id: number;
 }
-const TrackerLine = ({ color }: TrackerLineProps) => {
+const TrackerLine = ({ state, id }: TrackerLineProps) => {
   return (
     <div className="w-6 h-4 sm:w-full sm:h-6 grid place-items-center">
       <div
         className={cn(
-          'w-0 h-4 sm:w-full sm:h-0 rounded-[1px] border-2 border-dashed ',
-          `border-${color}`,
+          'w-0 h-4 sm:w-full sm:h-0 rounded-[1px] border-2 border-solid sm:border-dashed border-gray-medium',
+          state === 1 && state >= id && 'border-attention',
+          state === 2 && state >= id && 'border-blue-additional-color',
+          state === 3 && state >= id && 'border-orange-additional-color',
+          state === 4 && state >= id && 'border-green-additional-color',
         )}
       ></div>
     </div>
