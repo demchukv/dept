43c053ab6e2a5cc260070eb7855f93c93f1e9152
Volodymyr Demchuk <demchukv@gmail.com>
2024-09-12 09:48:30 +0300

    add page /task/add-task

--- app/[locale]/(protected)/task/add-task/page.tsx
+++ app/[locale]/(protected)/task/add-task/page.tsx
@@ -4,6 +4,9 @@ import React from 'react';
 import initTranslations from '@/app/i18n';
 import TranslationsProvider from '@/components/translations-provider';
 import { AddTaskForm } from '@/app/components/task/add-task-form';
+import { Icon } from '@/components/utils/icon';
+import Link from 'next/link';
+import { Card } from '@/app/components/card/card';
 
 const i18nNamespaces = ['task'];
 export const metadata: Metadata = {
@@ -22,7 +25,20 @@ const AddTaskPage = async ({
       locale={locale}
       resources={resources}
     >
-      <AddTaskForm />
+      <div className="flex gap-3 items-center mb-6 sm:mb-4">
+        <Link href="/task">
+          <Icon iconName="ArrowBack" width={24} height={24} />
+        </Link>
+        <h1 className="font-bold text-2xl leading-none text-main-dark ">
+          Мої заявки / задачі
+        </h1>
+      </div>
+      <h2 className="font-semibold text-base leading-normal text-main-dark mb-3 sm:mb-4">
+        Нова заявка
+      </h2>
+      <Card className="shadow-[6px_6px_40px_0_rgba(89,125,137,0.1)]">
+        <AddTaskForm />
+      </Card>
     </TranslationsProvider>
   );
 };
