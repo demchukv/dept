63bee1a98ed6b6b62a33440c5db5d3d476b3e2c7
Volodymyr Demchuk <demchukv@gmail.com>
2024-09-15 12:47:57 +0300

    start server page

--- app/components/products/server/server.tsx
+++ app/components/products/server/server.tsx
@@ -1,3 +1,15 @@
 export const Server = () => {
-  return <div>Server</div>;
+  return (
+    <>
+      <h1 className="font-bold text-2xl leading-none text-main-dark mb-4 lg:mb-8">
+        Мої продукти. Сервери і хостинг
+      </h1>
+      <h2 className="font-semibold text-base leading-normal text-main-dark mb-4">
+        Активні
+      </h2>
+      <h2 className="font-semibold text-base leading-normal text-main-dark mb-4">
+        Скасовані
+      </h2>
+    </>
+  );
 };
