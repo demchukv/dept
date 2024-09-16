562b6d2e42300a36b1b5f0b05d332db65bae126c
Volodymyr Demchuk <demchukv@gmail.com>
2024-09-16 12:39:56 +0300

    set carousel

--- app/components/products/server/server-change-tariff.tsx
+++ app/components/products/server/server-change-tariff.tsx
@@ -10,6 +10,33 @@ import {
   type CarouselApi,
 } from '@/components/ui/carousel-tariff';
 import { useEffect, useState } from 'react';
+import { Button } from '@/components/ui/button';
+import { cn } from '@/lib/utils';
+
+const tariffs = [
+  {
+    id: 1,
+    title: 'Тариф від 2 місяців',
+    price: 1000,
+  },
+  {
+    id: 2,
+    title: 'Тариф від 3 місяців',
+    price: 950,
+  },
+  {
+    id: 3,
+    title: 'Тариф від 6 місяців',
+    price: 900,
+  },
+  {
+    id: 4,
+    title: 'Тариф від 8 місяців',
+    price: 850,
+  },
+];
+
+const CARD_WIDTH = 280;
 
 interface ServerChangeTariffProps {
   data: ServerType;
@@ -31,7 +58,7 @@ export const ServerChangeTariff = ({ data }: ServerChangeTariffProps) => {
       setCurrent(api.selectedScrollSnap() + 1);
     });
   }, [api]);
-
+  console.log('count: ', count, 'current: ', current);
   return (
     <>
       <Separator className="my-4" />
@@ -44,25 +71,57 @@ export const ServerChangeTariff = ({ data }: ServerChangeTariffProps) => {
         тариф.
       </p>
 
-      <div className="mx-auto max-w-xs">
-        <Carousel setApi={setApi} className="w-full max-w-xs">
-          <CarouselContent>
-            {Array.from({ length: 5 }).map((_, index) => (
-              <CarouselItem key={index}>
-                <div>
-                  <div className="flex aspect-square items-center justify-center p-6">
-                    <span className="text-4xl font-semibold">{index + 1}</span>
-                  </div>
+      <Carousel setApi={setApi} className="w-full">
+        <CarouselContent>
+          {tariffs.map((item, index) => (
+            <CarouselItem
+              key={index}
+              className={cn(`pl-4 max-w-[${CARD_WIDTH}px] w-full`)}
+            >
+              <div className="border border-main-color rounded-[6px]">
+                <div className="flex flex-col gap-4 items-center justify-center p-6">
+                  <div className="text-4xl font-semibold">{item.id}</div>
+                  <div className="text-2xl">{item.title}</div>
+                  <div className="text-2xl">{item.price} грн</div>
                 </div>
-              </CarouselItem>
-            ))}
-          </CarouselContent>
-          <CarouselPrevious />
-          <CarouselNext />
-        </Carousel>
-        <div className="py-2 text-center text-sm text-muted-foreground">
-          Slide {current} of {count}
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        {/* <CarouselPrevious />
+        <CarouselNext /> */}
+      </Carousel>
+      <div className="flex justify-between items-center gap-2 mt-4">
+        <Button
+          type="button"
+          variant="ghost"
+          onClick={() => api?.canScrollPrev() && api?.scrollPrev()}
+          disabled={!api?.canScrollPrev()}
+        >
+          Prev
+        </Button>
+        <div className="flex gap-2 items-center justify-center">
+          {Array.from({ length: tariffs.length }).map((_, index) => (
+            <Button
+              type="button"
+              variant="ghost"
+              key={index}
+              onClick={() => api?.scrollTo(index)}
+              className={cn(
+                'w-3 h-3 rounded-full bg-main-color',
+                current === index + 1 && 'w-5 h-5',
+              )}
+            ></Button>
+          ))}
         </div>
+        <Button
+          type="button"
+          variant="ghost"
+          onClick={() => api?.canScrollNext() && api?.scrollNext()}
+          disabled={!api?.canScrollNext()}
+        >
+          Next
+        </Button>
       </div>
     </>
   );
