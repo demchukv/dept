75ca1148df06ff7931dc4677534dff29b6c24932
Volodymyr Demchuk <demchukv@gmail.com>
2024-09-16 16:44:31 +0300

    set carousel

--- app/components/products/server/server-change-tariff.tsx
+++ app/components/products/server/server-change-tariff.tsx
@@ -9,7 +9,7 @@ import {
   CarouselPrevious,
   type CarouselApi,
 } from '@/components/ui/carousel-tariff';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Button } from '@/components/ui/button';
 import { cn } from '@/lib/utils';
 
@@ -42,23 +42,31 @@ interface ServerChangeTariffProps {
   data: ServerType;
 }
 export const ServerChangeTariff = ({ data }: ServerChangeTariffProps) => {
+  const carouselRef = useRef<HTMLDivElement>(null);
   const [api, setApi] = useState<CarouselApi>();
-  const [current, setCurrent] = useState(0);
-  const [count, setCount] = useState(0);
+  const [inContainer, setInContainer] = useState(false);
+  const [contentWidth, setContentWidth] = useState(0);
+  const [containerWidth, setContainerWidth] = useState(0);
 
   useEffect(() => {
     if (!api) {
       return;
     }
 
-    setCount(api.scrollSnapList().length);
-    setCurrent(api.selectedScrollSnap() + 1);
+    const handleResize = () => {
+      setContainerWidth(api?.rootNode().clientWidth + 24 ?? 0);
+      setContentWidth(api?.containerNode().scrollWidth - 1 ?? 0);
+      setInContainer(containerWidth >= contentWidth ? false : true);
+    };
 
     api.on('select', () => {
-      setCurrent(api.selectedScrollSnap() + 1);
+      handleResize();
     });
-  }, [api]);
-  console.log('count: ', count, 'current: ', current);
+    window.addEventListener('resize', handleResize);
+    handleResize();
+    return () => window.removeEventListener('resize', handleResize);
+  }, [api, containerWidth, contentWidth]);
+
   return (
     <>
       <Separator className="my-4" />
@@ -70,59 +78,60 @@ export const ServerChangeTariff = ({ data }: ServerChangeTariffProps) => {
         користування послугами та без необхідності перенесення даних на інший
         тариф.
       </p>
-
-      <Carousel setApi={setApi} className="w-full">
-        <CarouselContent>
-          {tariffs.map((item, index) => (
-            <CarouselItem
-              key={index}
-              className={cn(`pl-4 max-w-[${CARD_WIDTH}px] w-full`)}
-            >
-              <div className="border border-main-color rounded-[6px]">
-                <div className="flex flex-col gap-4 items-center justify-center p-6">
-                  <div className="text-4xl font-semibold">{item.id}</div>
-                  <div className="text-2xl">{item.title}</div>
-                  <div className="text-2xl">{item.price} грн</div>
+      <div ref={carouselRef}>
+        <Carousel setApi={setApi} className="w-full">
+          <CarouselContent>
+            {tariffs.map((item, index) => (
+              <CarouselItem
+                key={index}
+                className={cn(`max-w-[${CARD_WIDTH}px] w-full`)}
+              >
+                <div className="border border-main-color rounded-[6px]">
+                  <div className="flex flex-col gap-4 items-center justify-center p-6">
+                    <div className="text-4xl font-semibold">{item.id}</div>
+                    <div className="text-2xl">{item.title}</div>
+                    <div className="text-2xl">{item.price} грн</div>
+                  </div>
                 </div>
-              </div>
-            </CarouselItem>
-          ))}
-        </CarouselContent>
-        {/* <CarouselPrevious />
-        <CarouselNext /> */}
-      </Carousel>
-      <div className="flex justify-between items-center gap-2 mt-4">
-        <Button
-          type="button"
-          variant="ghost"
-          onClick={() => api?.canScrollPrev() && api?.scrollPrev()}
-          disabled={!api?.canScrollPrev()}
-        >
-          Prev
-        </Button>
-        <div className="flex gap-2 items-center justify-center">
-          {Array.from({ length: tariffs.length }).map((_, index) => (
-            <Button
-              type="button"
-              variant="ghost"
-              key={index}
-              onClick={() => api?.scrollTo(index)}
-              className={cn(
-                'w-3 h-3 rounded-full bg-main-color',
-                current === index + 1 && 'w-5 h-5',
-              )}
-            ></Button>
-          ))}
-        </div>
-        <Button
-          type="button"
-          variant="ghost"
-          onClick={() => api?.canScrollNext() && api?.scrollNext()}
-          disabled={!api?.canScrollNext()}
-        >
-          Next
-        </Button>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+        </Carousel>
       </div>
+      {inContainer && (
+        <div className="flex justify-between items-center gap-2 mt-4">
+          <Button
+            type="button"
+            variant="ghost"
+            onClick={() => api?.canScrollPrev() && api?.scrollPrev()}
+            disabled={!api?.canScrollPrev()}
+          >
+            Prev
+          </Button>
+          <div className="flex gap-2 items-center justify-center">
+            {Array.from({ length: tariffs.length }).map((_, index) => (
+              <Button
+                type="button"
+                variant="ghost"
+                key={index}
+                onClick={() => api?.scrollTo(index)}
+                className={cn(
+                  'w-3 h-3 rounded-full bg-main-color',
+                  api?.selectedScrollSnap() === index && 'w-5 h-5',
+                )}
+              ></Button>
+            ))}
+          </div>
+          <Button
+            type="button"
+            variant="ghost"
+            onClick={() => api?.canScrollNext() && api?.scrollNext()}
+            disabled={!api?.canScrollNext()}
+          >
+            Next
+          </Button>
+        </div>
+      )}
     </>
   );
 };
