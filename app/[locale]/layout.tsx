454da4f02225724903db19e289fbfd447cd80b64
Volodymyr Demchuk <demchukv@gmail.com>
2024-09-18 19:35:02 +0300

    add top loader

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -48,7 +48,7 @@ export default async function RootLayout({
         )}
       >
         <NextTopLoader
-          color="#12b2e7"
+          color="var(--main-color)"
           initialPosition={0.08}
           crawlSpeed={200}
           height={3}
