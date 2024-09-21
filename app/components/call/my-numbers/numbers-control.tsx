180935798f80cc4d097c51c20c8f59411bef0c36
Volodymyr Demchuk <demchukv@gmail.com>
2024-09-21 18:15:37 +0300

    add continue and delete functions to control numbers

--- app/components/call/my-numbers/numbers-control.tsx
+++ app/components/call/my-numbers/numbers-control.tsx
@@ -14,7 +14,9 @@ import Link from 'next/link';
 import { cn } from '@/lib/utils';
 import { Checkbox } from '@/components/ui/checkbox';
 import { MyNumberFilterList } from '@/app/components/call/my-numbers/my-numbers-filter-list';
-import { useState } from 'react';
+import { startTransition, useState } from 'react';
+import { toast } from '@/components/ui/use-toast';
+import { Info } from '@/app/components/common/info';
 
 interface NumbersControlProps {
   data: PhoneNumbers[];
@@ -23,6 +25,7 @@ export const NumbersControl = ({ data }: NumbersControlProps) => {
   const [phoneNumbers, setPhoneNumbers] = useState<PhoneNumbers[]>(data);
   const [sortKey, setSortKey] = useState('numberType');
   const [checkAll, setCheckAll] = useState(false);
+  const [checkedNumbers, setCheckedNumbers] = useState<number[]>([]);
 
   const onSort = (key: string) => {
     if (key === sortKey) {
@@ -46,9 +49,66 @@ export const NumbersControl = ({ data }: NumbersControlProps) => {
   };
 
   const onCheckAllNumbers = (checked: boolean) => {
+    setCheckedNumbers(checked ? phoneNumbers.map((item) => item.id) : []);
     setCheckAll(checked);
   };
 
+  const continueNumbers = (key?: number) => {
+    startTransition(() => {
+      //TODO: make API request and setData
+      // const newData = getJson('/data/call-summary.json');
+      let values;
+      if (key) {
+        values = {
+          phoneNumbers: [key],
+          action: 'continueNumbers',
+        };
+      } else {
+        values = {
+          phoneNumbers: checkedNumbers,
+          action: 'continueNumbers',
+        };
+      }
+      toast({
+        title: 'Ви відправили наступні значення:',
+        description: (
+          <pre className="mt-2 w-full rounded-md bg-slate-950 p-4">
+            <code className="text-white">
+              {JSON.stringify(values, null, 2)}
+            </code>
+          </pre>
+        ),
+      });
+    });
+  };
+  const deleteNumbers = (key?: number) => {
+    startTransition(() => {
+      //TODO: make API request and setData
+      // const newData = getJson('/data/call-summary.json');
+      let values;
+      if (key) {
+        values = {
+          phoneNumbers: [key],
+          action: 'continueNumbers',
+        };
+      } else {
+        values = {
+          phoneNumbers: checkedNumbers,
+          action: 'continueNumbers',
+        };
+      }
+      toast({
+        title: 'Ви відправили наступні значення:',
+        description: (
+          <pre className="mt-2 w-full rounded-md bg-slate-950 p-4">
+            <code className="text-white">
+              {JSON.stringify(values, null, 2)}
+            </code>
+          </pre>
+        ),
+      });
+    });
+  };
   return (
     <>
       <div className="flex justify-between items-center gap-4 mb-5 sm:mb-9">
@@ -85,13 +145,18 @@ export const NumbersControl = ({ data }: NumbersControlProps) => {
           </label>
         </div>
         <div className="w-full sm:w-auto flex flex-col sm:flex-row items-center gap-3 sm:gap-9">
-          <Button type="button" className="w-full sm:w-auto">
+          <Button
+            type="button"
+            className="w-full sm:w-auto"
+            onClick={() => continueNumbers()}
+          >
             Подовжити
           </Button>
           <Button
             type="button"
             variant="ghost"
             className="text-warning w-full sm:w-auto gap-2"
+            onClick={() => deleteNumbers()}
           >
             <Icon
               iconName="Trash"
@@ -126,10 +191,19 @@ export const NumbersControl = ({ data }: NumbersControlProps) => {
                 <Checkbox
                   id="phoneNumber"
                   className="flex-shrink-0"
-                  checked={checkAll}
+                  checked={checkedNumbers.includes(item.id)}
+                  onCheckedChange={(checked: boolean) => {
+                    if (checked) {
+                      setCheckedNumbers([...checkedNumbers, item.id]);
+                    } else {
+                      setCheckedNumbers(
+                        checkedNumbers.filter((id) => id !== item.id),
+                      );
+                    }
+                  }}
                 />
                 <AccordionTrigger className="p-0 gap-1 sm:gap-9 flex-grow w-full">
-                  <div className="flex flex-col gap-2.5 sm:gap-0 sm:flex-row justify-between w-full items-start sm:items-center">
+                  <div className="w-full flex flex-col gap-2.5 sm:gap-0 sm:flex-row justify-between items-start sm:items-center">
                     <div className="flex gap-4 items-center">
                       <div
                         className={cn(
@@ -155,7 +229,7 @@ export const NumbersControl = ({ data }: NumbersControlProps) => {
                         />
                       </div>
                     </div>
-                    <div className="flex flex-row-reverse sm:flex-row justify-between sm:justify-end gap-5 items-center w-full sm:w-[50%]">
+                    <div className="flex flex-row-reverse sm:flex-row justify-between sm:justify-end gap-5 items-center w-full">
                       <div className="text-right font-normal text-sm sm:text-base text-main-dark leading-main-lh">
                         {isNaN(Number(item.priceForMonth))
                           ? item.priceForMonth
@@ -169,12 +243,78 @@ export const NumbersControl = ({ data }: NumbersControlProps) => {
                       <div className="font-normal text-sm sm:text-base text-main-dark leading-main-lh text-right">
                         до {format(item.activeTo, 'yyyy-MM-dd')}
                       </div>
+                      <span
+                        className="text-warning hover:text-main-dark hidden sm:block w-6 h-6 cursor-pointer"
+                        onClick={(e) => {
+                          deleteNumbers(item.id);
+                          e.stopPropagation();
+                        }}
+                      >
+                        <Icon
+                          iconName="Trash"
+                          width={24}
+                          height={24}
+                          className="w-6 h-6"
+                        />
+                      </span>
                     </div>
                   </div>
                 </AccordionTrigger>
               </div>
               <AccordionContent className="border-t mt-8 pt-8">
-                <p>{item.number}</p>
+                <p className="font-semibold text-base mb-4">
+                  Додаткові дії з номером
+                </p>
+                <div className="grid grid-cols-1 sm:grid-cols-[auto_auto] gap-y-2 sm:gap-y-4 sm:gap-x-4  place-content-start">
+                  <Button
+                    type="button"
+                    className="w-full sm:w-auto"
+                    onClick={() => continueNumbers(item.id)}
+                  >
+                    Подовжити
+                  </Button>
+                  <Info className="mt-0 mb-2">
+                    Подовжити передплату за використання поточною sim-картою зі
+                    збереженням всіх внесених налаштувань. Цей номер
+                    залишатиметься активним на новий обраний період.
+                  </Info>
+                  <Button type="button" className="w-full sm:w-auto" disabled>
+                    Активувати зберігання
+                  </Button>
+                  <Info className="mt-0 mb-2">
+                    В разі активації зберігання sim-карта буде вилучена із
+                    обладнання і відправлена на зберігання для оперативного
+                    підключення в майбутньому. Вартість зберігання sim-карти
+                    складає %% грн/міс
+                  </Info>
+                  <Button type="button" className="w-full sm:w-auto" disabled>
+                    Вийняти зі шлюза
+                  </Button>
+                  <Info className="mt-0 mb-2">
+                    Sim-карта буде вилучена з обладнання і відправлена фізично
+                    адміністратору поштовою службою. Термін відправки - 1
+                    робочий день. Після відправки ви отримаєте сповіщення на
+                    номер телефону, зазначений в профілі.
+                  </Info>
+                  <Button type="button" className="w-full sm:w-auto">
+                    Передати
+                  </Button>
+                  <Info className="mt-0 mb-2">
+                    Для зміни власника номера (трансферу) на електронну адресу
+                    поточного власника буде направлено листа з підтвердженням
+                    передачі. Після підтвердження дії номер з&#39;явиться у
+                    списку номерів в особистому кабінеті нового власника.
+                  </Info>
+                  <Button
+                    type="button"
+                    variant="ghost"
+                    className="w-full sm:w-auto text-warning hover:text-main-dark gap-2 sm:hidden"
+                    onClick={() => deleteNumbers(item.id)}
+                  >
+                    <Icon iconName="DeleteCircle" width={20} height={20} />
+                    Видалити номер
+                  </Button>
+                </div>
               </AccordionContent>
             </AccordionItem>
           </Accordion>
