d95567c4fcebee51dbe39386a33f6d64668c0741
Volodymyr Demchuk <demchukv@gmail.com>
2024-09-28 09:27:46 +0300

    add navigation events

--- app/[locale]/(protected)/dashboard/page.tsx
+++ app/[locale]/(protected)/dashboard/page.tsx
@@ -20,15 +20,7 @@ export default async function HomePage({
       locale={locale}
       resources={resources}
     >
-      <Header />
-
-      <div className="flex-grow flex flex-row-reverse ">
-        <main className="flex-grow px-4 py-6 sm:py-5 lg:px-[30px] lg:py-6 lg:ml-[234px] mt-[63px] md:mt-[69px] ">
-          <Dashboard />
-        </main>
-
-        <Aside />
-      </div>
+      <Dashboard />
     </TranslationsProvider>
   );
 }
