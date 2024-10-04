d97de00aaf294d841025b0543e0c1386711c7eb7
Volodymyr Demchuk <demchukv@gmail.com>
2024-10-04 16:52:10 +0300

    start integration page

--- app/components/settings/integration/integration.tsx
+++ app/components/settings/integration/integration.tsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { Card } from '@/app/components/card/card';
+import { Info } from '../../common/info';
+import { Separator } from '@/components/ui/separator';
 
 export const Integration = () => {
   return (
@@ -8,6 +11,43 @@ export const Integration = () => {
           Налаштування. Інтеграції
         </h1>
       </div>
+      <Card className="shadow-[6px_6px_40px_0_rgba(89,125,137,0.1)] p-4 md:p-8 mb-9">
+        <div className="flex flex-col sm:flex-row items-start sm:items-center justifay-between gap-6">
+          <div className="text-base font-semibold leading-normal">Заявки</div>
+          <Info className="text-sm text-main-dark mt-0 sm:items-center">
+            Підключити імпорт тікетів із вашої системи. Наприклад, якщо ви
+            ведете роботу у своєму корпоративному середовищі і не хочете
+            відволікатись на створення тікета.
+          </Info>
+        </div>
+        <Separator className="my-8" />
+      </Card>
+
+      <Card className="shadow-[6px_6px_40px_0_rgba(89,125,137,0.1)] p-4 md:p-8 mb-9">
+        <div className="flex flex-col sm:flex-row items-start sm:items-center justifay-between gap-6">
+          <div className="text-base font-semibold leading-normal">
+            Датацентри
+          </div>
+          <Info className="text-sm text-main-dark mt-0 sm:items-center">
+            Підключити керування послугами, які замовлені в сторонніх
+            провайдерів.
+          </Info>
+        </div>
+        <Separator className="my-8" />
+      </Card>
+
+      <Card className="shadow-[6px_6px_40px_0_rgba(89,125,137,0.1)] p-4 md:p-8">
+        <div className="flex flex-col sm:flex-row items-start sm:items-center justifay-between gap-6">
+          <div className="text-base font-semibold leading-normal">
+            Телефонія
+          </div>
+          <Info className="text-sm text-main-dark mt-0 sm:items-center">
+            Підключити телефонну систему для вашого бізнесу, щоб приймати та
+            здійснювати дзвінки через інтернет.
+          </Info>
+        </div>
+        <Separator className="my-8" />
+      </Card>
     </>
   );
 };
