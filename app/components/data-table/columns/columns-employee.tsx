8ea6fcd54161da5e5767791d8e4d35779a3291ec
Volodymyr Demchuk <demchukv@gmail.com>
2024-10-07 11:00:16 +0300

    employee data table

--- app/components/data-table/columns/columns-employee.tsx
+++ app/components/data-table/columns/columns-employee.tsx
@@ -42,28 +42,51 @@ export const columns: ColumnDef<employeeType>[] = [
       const email = row.original.email;
       return (
         <>
-          <div className="grid grid-cols-[3fr_1fr_2fr_auto] py-2.5 gap-2">
-            <div>
-              <div className="text-base text-main-dark font-semibold">
-                {name}
+          <div className="grid grid-cols-1 sm:grid-cols-[3fr_1fr_2fr_auto] sm:gap-2 border border-gray-light rounded-[6px] sm:border-0 sm:py-2.5 mb-4 sm:mb-0">
+            <div className="bg-bg-color sm:bg-transparent p-2 sm:p-0 border-b border-gray-light sm:border-0 flex items-center gap-2 justify-between">
+              <div>
+                <div className="text-base text-main-dark font-semibold">
+                  {name}
+                </div>
+                <div
+                  className={cn(
+                    'text-sm text-gray-dark font-medium',
+                    role === 'Адміністратор' && 'text-green-additional-color',
+                    role === 'Фінансист' && 'text-orange-additional-color',
+                    role === 'Менеджер проєкту' && 'text-blue-additional-color',
+                  )}
+                >
+                  {role}
+                </div>
               </div>
-              <div
-                className={cn(
-                  'text-sm text-gray-dark font-medium',
-                  role === 'Адміністратор' && 'text-green-additional-color',
-                  role === 'Фінансист' && 'text-orange-additional-color',
-                  role === 'Менеджер проєкту' && 'text-blue-additional-color',
-                )}
-              >
-                {role}
+              <div className="sm:hidden">
+                <Button
+                  type="button"
+                  variant="ghost"
+                  className="sm:hidden text-main-color hover:text-main-dark"
+                  onClick={() => {
+                    table.options.meta?.removeUserRole(Number(id));
+                  }}
+                >
+                  <Icon
+                    iconName="SettingAlert"
+                    width={24}
+                    height={24}
+                    className="w-6 h-6"
+                  />
+                </Button>
               </div>
             </div>
-            <div className="text-sm text-gray-dark">{phone}</div>
-            <div className="text-sm text-gray-dark">{email}</div>
+            <div className="text-sm text-main-dark p-2 sm:p-0  border-b border-gray-light sm:border-0">
+              {phone}
+            </div>
+            <div className="text-sm text-main-dark bg-bg-color sm:bg-transparent p-2 sm:p-0">
+              {email}
+            </div>
             <Button
               type="button"
               variant="ghost"
-              className="text-main-color hover:text-main-dark"
+              className="hidden sm:flex text-main-color hover:text-main-dark"
               onClick={() => {
                 table.options.meta?.removeUserRole(Number(id));
               }}
