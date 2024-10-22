dd74aebcaf9ebaca1f42d9550a10faeaed901278
Volodymyr Demchuk <demchukv@gmail.com>
2024-10-22 17:17:07 +0300

    electronic page

--- app/components/electronic/credentials.tsx
+++ app/components/electronic/credentials.tsx
@@ -3,6 +3,7 @@ import { electronicType, electronicCharacteristic } from '@/types/electronic';
 import { Button } from '@/components/ui/button';
 import { ElectronicInfoPhoto } from '@/app/components/electronic/electronic-info-photo';
 import { KeyValText } from '../common/key-val-text';
+import { Separator } from '@/components/ui/separator';
 
 interface CredentialsProps {
   data: electronicType;
@@ -10,42 +11,61 @@ interface CredentialsProps {
 export const Credentials = ({ data }: CredentialsProps) => {
   return (
     <>
-      <div className="w-full flex flex-col gap-4 sm:flex-row sm:gap-4 md:gap-16 mb-8">
+      <div className="w-full flex flex-col gap-4 sm:flex-row sm:gap-6 md:gap-16 mb-8">
         <div className="sm:max-w-2/3">
           <ElectronicInfoPhoto data={data} />
         </div>
         <div className="flex flex-col sm:flex-row gap-6">
-          <div>
-            <p className="font-semibold leading-main-lh mb-4">Характеристики</p>
-            {data.characteristics && Array.isArray(data.characteristics) && (
-              <div className="flex flex-col gap-2">
-                {data.characteristics.map(
-                  (item: electronicCharacteristic, ind) => (
-                    <KeyValText
-                      key={ind}
-                      k={item.label}
-                      val={item.value}
-                      className="justify-between"
-                      keyClass="font-medium text-xs"
-                      valClass="text-right whitespace-nowrap font-bold text-xs"
-                    />
-                  ),
-                )}
-              </div>
-            )}
+          <div className="flex flex-col justify-between">
+            <div>
+              <p className="font-semibold leading-main-lh mb-4">
+                Характеристики
+              </p>
+              {data.characteristics && Array.isArray(data.characteristics) && (
+                <div className="flex flex-col gap-2">
+                  {data.characteristics.map(
+                    (item: electronicCharacteristic, ind) => (
+                      <KeyValText
+                        key={ind}
+                        k={item.label}
+                        val={item.value}
+                        className="justify-between"
+                        keyClass="font-medium text-xs"
+                        valClass="text-right whitespace-nowrap font-bold text-xs"
+                      />
+                    ),
+                  )}
+                </div>
+              )}
+            </div>
+            <p className="hidden sm:block">Оціночна вартість</p>
           </div>
-          <div>
-            <p className="font-semibold leading-main-lh mb-4">Комплектність</p>
-            {data.complect && Array.isArray(data.complect) && (
-              <ul className="flex flex-col gap-2">
-                {data.complect.map((item, ind) => (
-                  <li key={ind} className="text-xs font-bold">
-                    {item}
-                  </li>
-                ))}
-              </ul>
-            )}
+          <Separator className="sm:hidden" />
+          <div className="flex flex-col justify-between">
+            <div>
+              <p className="font-semibold leading-main-lh mb-4">
+                Комплектність
+              </p>
+              {data.complect && Array.isArray(data.complect) && (
+                <ul className="flex flex-col gap-2">
+                  {data.complect.map((item, ind) => (
+                    <li key={ind} className="text-xs font-bold">
+                      {item}
+                    </li>
+                  ))}
+                </ul>
+              )}
+            </div>
+            <p className="hidden sm:block">{data.cost} грн</p>
           </div>
+          <Separator className="sm:hidden" />
+          <KeyValText
+            k="Оціночна вартість:"
+            val={`${data.cost} грн`}
+            keyClass="text-base font-semibold"
+            valClass="text-base font-semibold"
+            className="sm:hidden mb-3  justify-between"
+          />
         </div>
       </div>
 
@@ -56,7 +76,7 @@ export const Credentials = ({ data }: CredentialsProps) => {
         <div className="w-full text-center sm:text-right">
           <Button
             variant="ghost"
-            className="px-0 text-warning font-semibold leading-main-lh"
+            className="px-0 text-warning text-base sm:text-sm font-semibold leading-main-lh"
           >
             Повернути пристрій
           </Button>
--- app/components/electronic/electronic-info-photo.tsx
+++ app/components/electronic/electronic-info-photo.tsx
@@ -21,49 +21,77 @@ export const ElectronicInfoPhoto = ({ data }: ElectronicInfoPhotoProps) => {
   }, [data.progress]);
   return (
     <>
-      <p className="font-semibold leading-main-lh mb-4">
-        Реальні фото пристрою
-      </p>
-      {data?.photoList && Array.isArray(data.photoList) && (
-        <ScrollArea className="min-w-full w-[200px] h-[94px] overflow-x-hidden pb-2 mb-8">
-          <ul className="flex flex-row gap-4">
-            {data.photoList?.map((item) => (
-              <li key={item.id} className="w-20 h-20 grid place-content-center">
-                <div className="w-14 h-14 object-contain">
-                  {item.imgSrc && (
-                    <ImageZoom
-                      src={item.imgSrc}
-                      alt={data.title}
-                      className="w-14 h-14 cursor-pointer"
-                    />
-                  )}
-                  {!item.imgSrc && (
-                    <Icon
-                      iconName="ImageDefault"
-                      width={56}
-                      height={56}
-                      className="fill-gray-medium"
-                    />
-                  )}
-                </div>
-              </li>
-            ))}
-          </ul>
-          <ScrollBar orientation="horizontal" forceMount={true} />
-        </ScrollArea>
-      )}
-      {data.serial && (
-        <KeyValText
-          k="Серійний номер:"
-          val={data.serial}
-          className="sm:hidden mb-3"
-        />
-      )}
-      <p className="mb-4">{data?.description}</p>
-      <p className="flex justify-between font-semibold mb-2 sm:mb-3 mt-2 sm:mt-0">
-        Стан пристрою <span className="font-normal">{data?.progressTitle}</span>
-      </p>
-      {data.progress && <Progress value={progress} className="w-full mb-2" />}
+      <div className="flex flex-col-reverse sm:flex-col">
+        <div>
+          <p className="font-semibold leading-main-lh mb-4">
+            Реальні фото пристрою
+          </p>
+          {data?.photoList && Array.isArray(data.photoList) && (
+            <ScrollArea className="min-w-full w-[200px] h-[94px] overflow-x-hidden pb-2 mb-8">
+              <ul className="flex flex-row gap-4">
+                {data.photoList?.map((item) => (
+                  <li
+                    key={item.id}
+                    className="w-20 h-20 grid place-content-center"
+                  >
+                    <div className="w-14 h-14 object-contain">
+                      {item.imgSrc && (
+                        <ImageZoom
+                          src={item.imgSrc}
+                          alt={data.title}
+                          className="w-14 h-14 cursor-pointer"
+                        />
+                      )}
+                      {!item.imgSrc && (
+                        <Icon
+                          iconName="ImageDefault"
+                          width={56}
+                          height={56}
+                          className="fill-gray-medium"
+                        />
+                      )}
+                    </div>
+                  </li>
+                ))}
+              </ul>
+              <ScrollBar orientation="horizontal" forceMount={true} />
+            </ScrollArea>
+          )}
+        </div>
+        <div className="flex flex-col sm:flex-col-reverse">
+          <div>
+            <KeyValText
+              k="В користуванні:"
+              val={`з ${data.fromDate}`}
+              className="sm:hidden mb-3 text-sm justify-between"
+            />
+            <KeyValText
+              k="Термін оренди:"
+              val={`до ${data.toDate}`}
+              className="sm:hidden mb-3 text-sm justify-between"
+            />
+            {data.serial && (
+              <KeyValText
+                k="Серійний номер:"
+                val={data.serial}
+                className="sm:hidden mb-3 text-sm justify-between sm:justify-start"
+              />
+            )}
+          </div>
+          <div className="flex flex-col-reverse sm:flex-col">
+            <p className="mb-4">{data?.description}</p>
+            <div>
+              <p className="flex justify-between font-semibold mb-2 sm:mb-3 mt-2 sm:mt-0">
+                Стан пристрою{' '}
+                <span className="font-normal">{data?.progressTitle}</span>
+              </p>
+              {data.progress && (
+                <Progress value={progress} className="w-full mb-2" />
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
       <Separator className="mt-4 sm:hidden" />
     </>
   );
